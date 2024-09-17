d945b792d0368a71d9cd5b7f750cd2f96b74bdd9
stevesensei <wxtalt@gmail.com>
2024-09-17 08:26:31 +0800

    CDN缓存刷新

---
+++ scripts/cdn-refresher/cdn_refresher.js
@@ -0,0 +1,18 @@
+/**
+ * 缓存刷新脚本
+ * @command `node qcloudcdn.js $SECRET_ID $SECRET_KEY` 
+ */
+const qcloudSDK = require('qcloud-cdn-node-sdk');
+
+qcloudSDK.config({
+  secretId: process?.argv[2],
+  secretKey: process?.argv[3]
+})
+
+qcloudSDK.request('RefreshCdnDir', {
+  // See https://cloud.tencent.com/document/api/228/3947
+  'dirs.0': 'https://lruihao.cn/',
+  'type': 2
+}, (res) => {
+  res.code && console.log(res)
+})
